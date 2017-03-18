d7161bc47e07db64f81cb175a68a33ac2ef791a9
Abhinav Mall <abhinav.mall@mitre10.co.nz>
2017-03-18 12:28:16 +0530

    config and pump changes

---
+++ config/config.js
@@ -0,0 +1,26 @@
+var config = {}
+
+var config.ruleLightOne_On = {}
+var config.ruleLightOne_Off = {}
+var config.ruleLightTwo_On = {}
+var config.ruleLightTwo_Off = {}
+var config.rulePump_On = {};
+var config.rulePump_Off = {};
+
+config.ruleLightOne_On.minute = 00;
+config.ruleLightOne_On.hour = 10;
+
+config.ruleLightOne_Off.minute = 00;
+config.ruleLightOne_Off.hour = 20;
+
+config.ruleLightTwo_On.minute = 00;
+config.ruleLightTwo_On.hour = 12;
+
+config.ruleLightTwo_Off.minute = 00;
+config.ruleLightTwo_Off.hour = 22;
+
+config.rulePump_On.minute = 00;
+config.rulePump_On.hour = 10;
+
+config.rulePump_Off.minute = 00;
+config.rulePump_Off.hour = 22;
