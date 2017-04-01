e210762510dc598d1177534f919bffda2a3aa066
Abhinav Mall <abhinav.mall@mitre10.co.nz>
2017-04-01 15:18:53 +0530

    modify devicejob

--- deviceJob.js
+++ deviceJob.js
@@ -1,17 +1,17 @@
 var schedule = require('node-schedule');
 
-module.exports.createDeviceOnOffJobs = function(device, ruleDeviceOn, ruleDeviceOff){
+module.exports.createDeviceOnOffJobs = function(device){
   //Schedule device on job
-  ruleDeviceOn.minute = device.onTime.minute;
-  ruleDeviceOn.hour = device.onTime.hour;
+  device.ruleDeviceOn.minute = device.onTime.minute;
+  device.ruleDeviceOn.hour = device.onTime.hour;
   schedule.scheduleJob(ruleDeviceOn, function(){
     console.log('Aquarium Lights - ' + new Date() + ' ' + device.name + ' turned on');
     device.gpio.writeSync(ON);
   });
 
   //Schedule device off job
-  ruleDeviceOff.minute = device.offTime.minute;
-  ruleDeviceOff.hour = device.offTime.hour;
+  device.ruleDeviceOff.minute = device.offTime.minute;
+  device.ruleDeviceOff.hour = device.offTime.hour;
   schedule.scheduleJob(ruleDeviceOff, function(){
     console.log('Aquarium Lights - ' + new Date() + ' ' + device.name + ' turned off');
     device.gpio.writeSync(OFF);
