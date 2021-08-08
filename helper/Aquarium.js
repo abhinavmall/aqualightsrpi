b4a8f2180e7c286bbf3214b0f45830241021e777
Abhinav Mall <abhinav.0.mall@gmail.com>
2021-08-08 20:37:19 +0530

    Changing pins for SSR

--- helper/Aquarium.js
+++ helper/Aquarium.js
@@ -2,15 +2,19 @@ const GPIO = require('onoff').Gpio;
 const config = require('../config/config.js');
 const Device = require('../device.js');
 
+const SSR_1 = 17;
+const SSR_2 = 27;
+const SSR_3 = 22;
+const SSR_4 = 23;
 //Create two lights corresponding to GPIO 23 & 24
-const light1 = new Device('Light One', new GPIO(14, 'out'), config.ruleLightOne_On, config.ruleLightOne_Off);
-const light2 = new Device('Light Two', new GPIO(15, 'out'), config.ruleLightTwo_On, config.ruleLightTwo_Off);
+const light1 = new Device('Light One', new GPIO(SSR_1, 'out'), config.ruleLightOne_On, config.ruleLightOne_Off);
+const light2 = new Device('Light Two', new GPIO(SSR_2, 'out'), config.ruleLightTwo_On, config.ruleLightTwo_Off);
 
 //Create pump corresponding to GPIO 25
-const pump = new Device('Pump', new GPIO(18, 'out'), config.rulePump_On, config.rulePump_Off);
+const pump = new Device('Pump', new GPIO(SSR_3, 'out'), config.rulePump_On, config.rulePump_Off);
 
 //Create wavemaker corresponding to 8
-const wavemaker = new Device('Wave Maker', new GPIO(23, 'out'), config.ruleWaveMaker_On, config.ruleWaveMaker_Off);
+const wavemaker = new Device('Wave Maker', new GPIO(SSR_4, 'out'), config.ruleWaveMaker_On, config.ruleWaveMaker_Off);
 
 //Create highpower light corresponding to 22
 //const powerLight = new Device('Power Light', new GPIO(22, 'out'), config.rulePowerLight_On, config.rulePowerLight_Off);
